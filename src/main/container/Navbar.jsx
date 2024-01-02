d24364e6ba655c853e423952da3f83d17f17095e
Anuraj-TR <anurajtr17@gmail.com>
2024-01-02 11:30:23 +0530

    user name section style updations

--- src/main/container/Navbar.jsx
+++ src/main/container/Navbar.jsx
@@ -1,9 +1,12 @@
 import { UserAuth } from "../context/AuthContext";
 import { RiLogoutCircleRLine } from "react-icons/ri";
+import { MdWavingHand } from "react-icons/md";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
   const { user, logOut } = UserAuth();
-  
+  const [userName, setUserName] = useState('')
+
   const handleLogOut = async () => {
     try {
       await logOut()
@@ -11,22 +14,35 @@ export const Navbar = () => {
       console.log(error);
     }
   }
+
+  useEffect(() => {
+    setUserName(user?.displayName.split(' ')[0])
+  },[user])
   
 
   return (
     <>
-      <div className="flex items-center justify-between w-full p-2">
-        <div className="w-2/3  p-3">
-          {user && `Hellow ${user?.displayName}`}
+      <div className="flex items-center justify-end w-full p-2">
+        <div className="inline-flex items-center capitalize p-3">
+          {userName && (
+            <>
+              <span>Hello {userName}</span>
+              <span className="ml-2">
+                <MdWavingHand size={25} color="#ffff00"/>
+              </span>
+            </>
+          )}
         </div>
-        <div className="flex-1 flex">
-          {user?.displayName && (
+        <div className="inline-flex">
+          {userName && (
             <button
               className="inline-flex items-center px-4 py-2 transition-all duration-300 capitalize text-primary bg-white hover:text-white hover:bg-primary hover:border border-primary rounded-md ml-auto"
               onClick={handleLogOut}
             >
               <span className="hidden md:inline-block md:mr-2">logout</span>
-              <span><RiLogoutCircleRLine /></span>
+              <span>
+                <RiLogoutCircleRLine />
+              </span>
             </button>
           )}
         </div>
