aba3564f706aff9d8994b16d9989415f0f299986
Anuraj-TR <anurajtr17@gmail.com>
2024-01-02 10:15:38 +0530

    added condition based on the location path in layout comp

--- src/main/container/Layout.jsx
+++ src/main/container/Layout.jsx
@@ -1,14 +1,16 @@
+import { useLocation } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
 
 export const Layout = ({ children }) => {
+  const location = useLocation().pathname;
   return (
     <>
       <div className="flex h-screen overflow-hidden justify-start relative md:p-2 lg:p-3">
         <Sidebar />
         <div className="w-full relative">
           <Navbar />
-          <div className="w-full h-full">{children}</div>
+          <div className="w-full h-full">{ location === '/signin' && children}</div>
         </div>
       </div>
     </>
