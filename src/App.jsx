7dbd62b5f06af43594b1265349255caeca2af906
Anuraj-TR <anurajtr17@gmail.com>
2024-01-03 12:31:09 +0530

    added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes,Route } from "react-router-dom";
 import { Layout } from "./main/container/Layout";
 import { SignUpPage } from "./main/pages/SignUpPage";
 import { AuthContextProvider } from "./main/context/AuthContext";
+import { ProtectedRoutes } from "./main/container/ProtectedRoutes";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <AuthContextProvider>
         <Layout>
           <Routes>
-            <Route path="/" element={<div>hello world!</div>} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<div>hello world!</div>} />
+            </Route>
             <Route path="/signin" element={<SignUpPage />} />
           </Routes>
         </Layout>
