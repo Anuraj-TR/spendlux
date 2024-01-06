31747adf5a38c9cf1850daec4ea575cb217f7ee7
Anuraj-TR <anurajtr17@gmail.com>
2024-01-06 17:44:16 +0530

    added base for styled components

--- src/styledComponents/atom/Title.jsx
+++ src/styledComponents/atom/Title.jsx
@@ -1,8 +1,17 @@
 import styled from "styled-components";
+import { devices } from "../base/StyledComponentbase";
 
 export const SubTitle = styled.h2`
   font-size: 20px;
   font-weight: 600;
   color: #27292c;
   text-decoration: none;
+
+  @media ${devices.tablet} {
+    font-size: 22px;
+  }
+
+  @media ${devices.tabletPro}{
+    font-size: 26px;
+  }
 `;
