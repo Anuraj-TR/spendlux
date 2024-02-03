6c853f094202157b6ad80d4370746e7a8c281ae6
Anuraj-TR <anurajtr17@gmail.com>
2024-02-03 15:49:23 +0530

    added safe to spend

--- src/main/modules/budget/components/BudgetChartCard.jsx
+++ src/main/modules/budget/components/BudgetChartCard.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useEffect, useState } from "react";
 import {
   ResponsiveContainer,
@@ -9,20 +10,28 @@ import {
 const BudgetChartCard = ({ data }) => {
   const [currentSpend, setCurrentSpend] = useState();
   const values = [{ name: "L1", value: currentSpend }];
+  const [safeToSpend, setSafeToSpend] = useState();
 
   useEffect(() => {
     setCurrentSpend(data[0].current_spend);
+    setSafeToSpend(() => {
+      const now = moment()
+      const end = moment().endOf('month')
+      const daysLeft = end.diff(now, "days");
+      const safe = (data[0].budget - data[0].current_spend) / daysLeft
+      return Math.round(safe);
+    })
   },[data])
 
   return (
     <>
       <div className="w-full max-w-[500px] text-base font-medium">
-        <ResponsiveContainer width="100%" height={200}>
+        <ResponsiveContainer width="100%" height={150}>
           <RadialBarChart
             width="100%"
             height="100%"
             cx="50%"
-            cy="70%"
+            cy="90%"
             innerRadius={100}
             outerRadius={120}
             barSize={50}
@@ -32,6 +41,7 @@ const BudgetChartCard = ({ data }) => {
           >
             <PolarAngleAxis
               type="number"
+              // TODO : Replace the domain values by variables
               domain={[0, 10000]}
               angleAxisId={0}
               tick={false}
@@ -89,6 +99,9 @@ const BudgetChartCard = ({ data }) => {
             </text>
           </RadialBarChart>
         </ResponsiveContainer>
+        <div className="w-full text-center text-lg font-medium text-primary">
+          safe to spend : <span className="text-xl font-semibold">{ safeToSpend }</span>
+        </div>
       </div>
     </>
   );
